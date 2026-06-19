78c2efd8a60e33973f714c3e32614fc4796de0e8
pedrothier-lab <pedro.thier@aluno.senai.br>
2026-06-19 20:51:41 -0300

    criado retangulo

---
+++ Untitled-1.py
@@ -0,0 +1,16 @@
+print("+", end = "")
+for c in range(2,20):
+    print("-", end="")
+print("+")
+
+for i in range(2,5):
+    print("|", end = "")
+    for c in range(2,20):
+        print(' ', end= "")
+    print('|')
+
+print("+", end="")
+for c in range(2,20):
+    print('-', end="")
+print('+')
+0
